4fd330a6e57990c61233f9f0d03f6c6387612b7d
Brett Mayson <brett@mayson.io>
2025-08-03 10:14:22 -0600

    fix rf compat

--- addons/compat_rf/CfgVehicles.hpp
+++ addons/compat_rf/CfgVehicles.hpp
@@ -1,10 +1,10 @@
 class CfgVehicles {
     class Helicopter_Base_F;
     class UAV_RC40_Base_RF: Helicopter_Base_F {
-        GVAR(mode) = "LOS";
-        GVAR(power)[] = { 100, 200, 300 };
-        GVAR(defaultPower) = 100;
-        GVAR(freq)[] = { 2400, 5800 };
-        GVAR(defaultFreq) = 2400;
+        EGVAR(uav,mode) = "LOS";
+        EGVAR(uav,power)[] = { 100, 200, 300 };
+        EGVAR(uav,defaultPower) = 100;
+        EGVAR(uav,freq)[] = { 2400, 5800 };
+        EGVAR(uav,defaultFreq) = 2400;
     }; 
 };
